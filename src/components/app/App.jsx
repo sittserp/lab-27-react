3c212e1eaf5469c930c4ab937df3752a25e7aef7
Perry Sittser <sittserp@gmail.com>
2021-02-02 15:29:23 -0800

    folders reorganized

---
+++ src/components/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AllCharacters from '../characters/AllCharacters';
+
+
+export default function App() {
+  return (
+    <>
+      <AllCharacters />
+    </>
+  );
+}
